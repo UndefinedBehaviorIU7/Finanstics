81846a6f69c477ecbf2275050f07f3d545f27d57
PS <jooonsnow@mail.ru>
2025-03-17 18:13:57 +0300

    color change and fix artifact version

--- app/src/main/java/com/example/finanstics/ui/theme/Color.kt
+++ app/src/main/java/com/example/finanstics/ui/theme/Color.kt
@@ -1,11 +1,11 @@
 package com.example.finanstics.ui.theme
 
-//import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color
 
-//val Purple = Color(0xFFD0BCFF)
-//val PurpleGrey = Color(0xFFCCC2DC)
-//val Pink = Color(0xFFEFB8C8)
-//
-//val Purple = Color(0xFF6650a4)
-//val PurpleGrey = Color(0xFF625b71)
-//val Pink = Color(0xFF7D5260)
+val PurpleLight = Color(0xFFD0BCFF)
+val PurpleGreyLight = Color(0xFFCCC2DC)
+val PinkLight = Color(0xFFEFB8C8)
+
+val PurpleDark = Color(0xFF6650a4)
+val PurpleGreyDark = Color(0xFF625b71)
+val PinkDark = Color(0xFF7D5260)
