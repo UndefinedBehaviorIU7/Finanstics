b466b9e4a6d6e5aa4161398fdfb3898f4f5cf8f9
PS <jooonsnow@mail.ru>
2025-03-25 20:27:52 +0300

    correction bars

--- app/src/main/java/com/example/finanstics/presentation/stats/Preview.kt
+++ app/src/main/java/com/example/finanstics/presentation/stats/Preview.kt
@@ -1,16 +1,24 @@
 package com.example.finanstics.presentation.stats
 
+import android.content.res.Configuration
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.finanstics.ui.theme.EXPENSES_DATA
+import com.example.finanstics.ui.theme.FinansticsTheme
 import com.example.finanstics.ui.theme.INCOMES_DATA
 
 @Suppress("MagicNumber")
-@Preview
+@Preview (
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+)
 @Composable
 fun StatsPreview() {
-    StatsView(
-        incomes = INCOMES_DATA,
-        expenses = EXPENSES_DATA
-    )
+    FinansticsTheme (
+        dynamicColor = false
+    ) {
+        StatsView(
+            incomes = INCOMES_DATA,
+            expenses = EXPENSES_DATA
+        )
+    }
 }
