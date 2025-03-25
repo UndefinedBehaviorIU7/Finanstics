7d40dc842d9283fac8f4677c497138838d37edd7
zgp23u184 <zgp23u184@student.bmstu.ru>
2025-03-25 18:09:13 +0300

    Added login view

---
+++ app/src/main/java/com/example/finanstics/presentation/login/Login.kt
@@ -0,0 +1,133 @@
+package com.example.finanstics.presentation.login
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.finanstics.presentation.forms.Form
+import com.example.finanstics.presentation.forms.ButtonForm
+import com.example.finanstics.R
+
+
+@Composable
+@Suppress("MagicNumber")
+fun Login(navController: NavController, vm: LoginViewModel = viewModel()) {
+    val uiState = vm.uiState.collectAsState().value
+
+    Box(
+        modifier = Modifier.fillMaxSize().background(MaterialTheme.colorScheme.secondary),
+        contentAlignment = Alignment.Center,
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+
+            Spacer(modifier = Modifier.weight(0.7f))
+
+            if (uiState !is LoginUiState.Success && uiState !is LoginUiState.Loading) {
+                Image(
+                    painter = painterResource(R.drawable.placeholder),
+                    modifier = Modifier.size(280.dp).weight(1f),
+                    contentDescription = ""
+                )
+            }
+
+            when (uiState) {
+                is LoginUiState.Idle -> {
+                    Column(
+                        modifier = Modifier.weight(1.2f)
+                    ) {
+
+                        Column(modifier = Modifier.padding(start = 60.dp, end = 60.dp)) {
+
+                            Form(value = uiState.login,
+                                label = stringResource(R.string.login),
+                                isError = false,
+                                lambda = { vm.loginChange(it) })
+
+                            Form(value = uiState.password,
+                                label = stringResource(R.string.password),
+                                isError = false,
+                                lambda = { vm.passwordChange(it) })
+                        }
+
+                        Spacer(modifier = Modifier.weight(0.5f))
+                    }
+
+                    ButtonForm(modifier = Modifier.weight(0.5f),
+                        buttonText = stringResource(R.string.login),
+                        navText = stringResource(R.string.create_account),
+                        action = {vm.auth()},
+                        navigate = TODO("nav")
+                    )
+
+                    Spacer(modifier = Modifier.weight(0.6f))
+                }
+
+                is LoginUiState.Error -> {
+                    Column(
+                        modifier = Modifier.weight(1.3f)
+                    ) {
+
+                        Column(modifier = Modifier.padding(start = 60.dp, end = 60.dp)) {
+                            Form (value = uiState.login,
+                                label = stringResource(R.string.tag),
+                                isError = true, lambda = TODO()
+                            )
+
+                            Form (value = uiState.password,
+                                label = stringResource(R.string.password),
+                                isError = true, lambda = TODO()
+                            )
+
+                            Spacer(modifier = Modifier.weight(0.5f))
+                            Text(
+                                text = "Error: ${uiState.errorMessage}",
+                                color = Color.Red,
+                                modifier = Modifier.align(Alignment.CenterHorizontally)
+                                    .weight(0.5f),
+                            )
+                        }
+                        Spacer(modifier = Modifier.weight(0.1f))
+                    }
+                    ButtonForm(modifier = Modifier.weight(0.5f),
+                        buttonText = stringResource(R.string.log_in),
+                        navText = stringResource(R.string.log_in),
+                        action = TODO(),
+                        navigate = TODO("nav")
+                    )
+
+                    Spacer(modifier = Modifier.weight(0.6f))
+                }
+
+                is LoginUiState.Loading -> {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        CircularProgressIndicator()
+                    }
+                }
+
+                is LoginUiState.Success -> {
+                    TODO("nav")
+                }
+            }
+        }
+    }
+}
