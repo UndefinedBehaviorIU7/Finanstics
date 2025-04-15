798fdfc3d720a0e9a77aba23f592ad07200595d5
PS <jooonsnow@mail.ru>
2025-04-15 15:17:02 +0300

    style

--- app/src/main/java/com/example/finanstics/db/FinansticsDatabase.kt
+++ app/src/main/java/com/example/finanstics/db/FinansticsDatabase.kt
@@ -7,7 +7,7 @@ import androidx.room.RoomDatabase
 
 @Database(
     entities = [Action::class, Category::class],
-    version = 2,
+    version = 3,
     exportSchema = false
 )
 abstract class FinansticsDatabase : RoomDatabase() {
