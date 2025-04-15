cb383c7979c6c35d8464e61db3b40831c6bc3570
Eremin Georgii <eremings@student.bmstu.ru>
2025-04-15 14:20:33 +0300

    start add task

---
+++ app/src/main/java/com/example/finanstics/presentation/addAction/AddActionUiState.kt
@@ -0,0 +1,41 @@
+package com.example.finanstics.presentation.addAction
+
+sealed class AddActionUiState {
+    data class Idle (
+        var typeAction: String,
+        var nameAction: String,
+        var moneyAction: Int,
+        var data: String,
+        var category: String,
+        var description: String,
+        var allCategory: List<String>,
+        val menuExpandedType: Boolean,
+        val menuExpandedCategory: Boolean
+    ): AddActionUiState()
+
+    data class Error (
+        var typeAction: String,
+        var nameAction: String,
+        var moneyAction: Int,
+        var data: String,
+        var category: String,
+        var description: String,
+        val errorMessage: String,
+        var errorField: List<String>,
+        var allCategory: List<String>,
+        val menuExpandedType: Boolean,
+        val menuExpandedCategory: Boolean
+    ): AddActionUiState()
+
+    data class Loading (
+        var typeAction: String,
+        var nameAction: String,
+        var moneyAction: Int,
+        var data: String,
+        var category: String,
+        var description: String,
+        var allCategory: List<String>,
+        val menuExpandedType: Boolean,
+        val menuExpandedCategory: Boolean
+    ): AddActionUiState()
+}
