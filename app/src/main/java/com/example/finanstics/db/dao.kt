c51b1feeccd20ffeefb683549810fa71f848e5a0
PS <jooonsnow@mail.ru>
2025-04-19 14:50:08 +0300

    style

--- app/src/main/java/com/example/finanstics/db/dao.kt
+++ app/src/main/java/com/example/finanstics/db/dao.kt
@@ -32,8 +32,8 @@ interface ActionDao {
 
     @Query(
         "SELECT * FROM actions WHERE strftime('%m', date) = printf('%02d', :month) " +
-                "AND strftime('%Y', date) = printf('%d', :year) " +
-                "AND strftime('%d', date) = printf('%02d', :day)"
+            "AND strftime('%Y', date) = printf('%d', :year) " +
+            "AND strftime('%d', date) = printf('%02d', :day)"
     )
     suspend fun getActionsByDate(month: Int, year: Int, day: Int): List<Action>
 
@@ -45,16 +45,16 @@ interface ActionDao {
 
     @Query(
         "SELECT * FROM actions " +
-                "WHERE strftime('%m', date) = printf('%02d', :month) " +
-                "AND strftime('%Y', date) = printf('%d', :year) " +
-                "AND type = 1"
+            "WHERE strftime('%m', date) = printf('%02d', :month) " +
+            "AND strftime('%Y', date) = printf('%d', :year) " +
+            "AND type = 1"
     )
     suspend fun getIncomesByMonthYear(month: Int, year: Int): List<Action>
 
     @Query(
         "SELECT * FROM actions WHERE strftime('%m', date) = printf('%02d', :month) " +
-                "AND strftime('%Y', date) = printf('%d', :year) " +
-                "AND type = 0"
+            "AND strftime('%Y', date) = printf('%d', :year) " +
+            "AND type = 0"
     )
     suspend fun getExpensesByMonthYear(month: Int, year: Int): List<Action>
 }
