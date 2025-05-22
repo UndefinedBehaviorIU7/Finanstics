520228bea3b0b919b0a361cb2949c0ed83212142
PS <jooonsnow@mail.ru>
2025-05-22 23:35:23 +0300

    style

--- app/src/main/java/com/ub/finanstics/presentation/MainActivity.kt
+++ app/src/main/java/com/ub/finanstics/presentation/MainActivity.kt
@@ -135,10 +135,9 @@ class MainActivity : ComponentActivity() {
 
     private fun askNotificationPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (
-                ContextCompat.checkSelfPermission(
-                this,
-                Manifest.permission.POST_NOTIFICATIONS
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.POST_NOTIFICATIONS
                 ) != PackageManager.PERMISSION_GRANTED
             ) {
                 requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
